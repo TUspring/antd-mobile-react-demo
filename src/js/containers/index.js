5fedc4b81c9ed9f0b5bbd4cf540f5be4b82a9c94
tuspring <441928788@qq.com>
2018-12-26 16:02:47 +0800

    reac-app

---
+++ src/js/containers/index.js
@@ -0,0 +1,17 @@
+export {default as App} from './App'
+
+// 首页
+export { default as Home } from './Home'
+
+// 详情
+export { default as Coupon } from './Coupon'
+export { default as CouponDetail } from './Coupon/Detail'
+
+// 文章
+export { default as Tour } from './Tour'
+
+// 用户
+export { default as User } from './User'
+
+// 404
+export { default as NotFoundPage } from './404.js'
